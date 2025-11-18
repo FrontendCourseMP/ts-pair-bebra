80efba1651e482a39d71ab9cb672ff5b0bc622c0
glushul <ulyana.glushchenko2031@mail.ru>
2025-11-18 15:31:11 +0300

    Глущенко, лр2

--- src/task2.ts
+++ src/task2.ts
@@ -1,20 +1,35 @@
-class Bracket {
+class BracketValidator {
 
-    bracketArray: { [key: string] : string} = {
+    bracketDict: { [key: string] : string} = {
         '(' : ')',
         '{' : '}',
         '[' : ']'
     }
 
     flag(bracketStr: string) : boolean {
+        const stack: string[] = [];
         for (let symb of bracketStr) {
-            if (this.bracketArray.keys.includes(symb)) {
-                return true
+            if (this.bracketDict.keys.includes(symb)) {
+               stack.push(symb);
             }
-            else {
-                return false
+            else if (this.bracketDict.values.includes(symb)) {
+                if (stack.length === 0)
+                    return false;
+                else {
+                    const last = stack.pop();
+                    if (!last) {
+                        return false;
+                    } else if (this.bracketDict[last] === symb){
+                        return false;
+                    } else {
+                        continue;
+                    }
+                } 
             }
+        }
+        if (stack.length === 0)
+            return true
+        else
+            return false
     }
-    return false
-}
 }
